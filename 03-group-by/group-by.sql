8094026fad4a8d0e8fb792d00069726329433fcf
Raphael Pieroni <raphael.ifusp@gmail.com>
2026-09-18 10:58:01 -0400

    Update group-by.sql

--- 03-group-by/group-by.sql
+++ 03-group-by/group-by.sql
@@ -1,1 +1,36 @@
 
+SELECT
+    customer_id,
+    SUM(amount) AS total_revenue
+FROM orders
+WHERE status = 'completed'
+GROUP BY customer_id;
+
+SELECT
+    status,
+    COUNT(*) AS n_orders
+FROM orders
+GROUP BY status;
+
+SELECT
+    status,
+    COUNT(*) AS number_of_orders,
+    SUM(amount) AS total_amount,
+    AVG(amount) AS average_amount
+FROM orders
+GROUP BY status;
+
+SELECT
+    customer_id,
+    SUM(amount) AS total_revenue
+FROM orders
+WHERE status = 'completed'
+GROUP BY customer_id
+
+SELECT
+    customer_id,
+    SUM(amount) AS total_revenue
+FROM orders
+WHERE status = 'completed'
+GROUP BY customer_id
+ORDER BY total_revenue DESC;
