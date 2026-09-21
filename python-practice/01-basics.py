a9f0a399ac002c69c20e27c0a797179b279149fb
Raphael Pieroni <raphael.ifusp@gmail.com>
2026-09-21 11:33:52 -0400

    Create 01-basics.py
    Add basic Python revenue exercise

---
+++ python-practice/01-basics.py
@@ -0,0 +1,9 @@
+order_amounts = [120, 85, 210, 95, 175]
+
+total_revenue = sum(order_amounts)
+number_of_orders = len(order_amounts)
+average_order_value = total_revenue / number_of_orders
+
+print(total_revenue)
+print(number_of_orders)
+print(average_order_value)
